337b22393f6abededcb2b8ff0615e8497f6a381e
Dani <lequodo@gmail.com>
2023-01-23 22:33:10 +0100

    Landscape orientation on mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  orientationLock();
   const btnAllPlay = document.querySelectorAll(".sound-effect")
   btnAllPlay.forEach(element => {
     element.addEventListener('click', function(e) {
@@ -9,4 +10,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 function playAudio(element) {
   document.getElementById(element.dataset.audio).play()
-} 
+}
+
+const orientationLock = () => {
+  if (screen.orientation.type.startsWith("portrait")) {
+    screen.orientation.lock("landscape-primary")
+        .then(() => console.log("Orientation locked"))
+        .catch((err) => console.error(`Error locking orientation: ${err.message}`));
+  }
+}
