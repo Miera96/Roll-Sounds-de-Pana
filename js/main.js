c6c16c6621896cbc54f4869710c905a7e18a2696
Dani <lequodo@gmail.com>
2023-01-23 22:56:04 +0100

    Remove screen orientation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  orientationLock();
   const btnAllPlay = document.querySelectorAll(".sound-effect")
   btnAllPlay.forEach(element => {
     element.addEventListener('click', function(e) {
@@ -10,12 +9,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 function playAudio(element) {
   document.getElementById(element.dataset.audio).play()
-}
-
-const orientationLock = () => {
-  if (screen.orientation.type.startsWith("portrait")) {
-    screen.orientation.lock("landscape-primary")
-        .then(() => console.log("Orientation locked"))
-        .catch((err) => console.error(`Error locking orientation: ${err.message}`));
-  }
 }
